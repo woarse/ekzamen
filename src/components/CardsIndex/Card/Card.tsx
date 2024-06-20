9d07cc5e74d374a7d7f326df198758c873a83ecb
Ilya <ilya.yohumming@icloud.com>
2024-06-20 09:25:48 +0600

    init

---
+++ src/components/CardsIndex/Card/Card.tsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+interface ICardProps {}
+
+const Card = () => {
+  return <div className={styles.card}></div>;
+};
+
+export default Card;
---
+++ src/components/CardsIndex/CardsIndex.tsx
@@ -0,0 +1,6 @@
+import styles from "./style.module.css";
+const CardsIndex = () => {
+  return <div className={styles.container}> `</div>;
+};
+
+export default CardsIndex;
